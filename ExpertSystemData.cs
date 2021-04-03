dae756a71c90863c3788aeb8bb28b4e038129e5b
Влад Логунов <vl3012@icloud.com>
2021-04-04 00:52:55 +0700

    Добавлены действия кнопок, список вопросов и ответов, добавление классов "Вопрос", "Ответ", добавлены веса для ответов

--- ExpertSystemData.cs
+++ ExpertSystemData.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WindowsFormsApp_Expert_System
 {
@@ -14,5 +16,7 @@ namespace WindowsFormsApp_Expert_System
         {
             propositions = new List<Proposition>();
         }
+
+        
     }
 }
